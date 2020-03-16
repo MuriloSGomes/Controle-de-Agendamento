49edf9b6235237f12b252f47e21072693230a72e
MuriloSGomes <seno.murilo@hotmail.com>
2020-03-15 21:09:35 -0300

    cria camada de negócio, adiciona entidades.

---
+++ ScheduleMedicControl.Business/Models/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduleMedicControl.Business.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo nome é obrigatório")]
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public string Cpf { get; set; }
+        public string  Email { get; set; }
+        public bool TemConvenio { get; set; }
+        public string NumeroConvenio { get; set; }
+        public string NomeConvenio { get; set; }
+    }
+}
---
+++ ScheduleMedicControl.Business/Models/Clinica.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduleMedicControl.Business.Models
+{
+    public class Clinica
+    {
+        public int Id { get; set; }
+        public string CNPJ { get; set; }
+        public string Telefone { get; set; }
+        public string Endereco { get; set; }
+    }
+}
