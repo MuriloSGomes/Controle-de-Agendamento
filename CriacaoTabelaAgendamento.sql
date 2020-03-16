66f53ffdbfd29eb97039cc6da99130de4d688954
Murilo Seno <seno.murilo@hotmail.com>
2020-03-16 18:57:53 -0300

    Update CriacaoTabelaAgendamento.sql
    altera script

--- CriacaoTabelaAgendamento.sql
+++ CriacaoTabelaAgendamento.sql
@@ -1,6 +1,6 @@
 create Table Agendamento (
-	atendimentoId int not null auto_increment,
-    atendimentoSituacao char(1),
-    atendimentoData date,
-    primary key (atendimentoId)
+    agendamentoId int not null auto_increment,
+    agendamentoSituacao char(1),
+    agendamentoData date,
+    primary key (agendamentoId)
 );
