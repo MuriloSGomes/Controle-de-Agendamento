d27054d235a987548c7f42dba3c93ac181218842
Murilo Seno <seno.murilo@hotmail.com>
2020-03-17 19:05:39 -0300

    Update CriacaoTabelaAgendamento.sql
    identação

--- CriacaoTabelaAgendamento.sql
+++ CriacaoTabelaAgendamento.sql
@@ -1,8 +1,8 @@
 create Table Agendamento (
 	agendamentoId int not null auto_increment,
-    agendamentoSituacao char(1),
-    agendamentoData date,
-    primary key (agendamentoId),
+        agendamentoSituacao char(1),
+        agendamentoData date,
+        primary key (agendamentoId),
 	foreign key (agendamentoId) references cliente(clienteId),
 	foreign key (agendamentoId) references clinica(clinicaId)
 );
