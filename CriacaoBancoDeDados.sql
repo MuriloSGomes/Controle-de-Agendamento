17e5f49b5275a235cf73836cce3069066235213c
MuriloSGomes <seno.murilo@hotmail.com>
2020-03-18 21:15:40 -0300

    Unifica script

---
+++ CriacaoBancoDeDados.sql
@@ -0,0 +1,35 @@
+create database controleAgendamento;
+
+use controleAgendamento;
+
+create Table Cliente (
+    clienteId int not null auto_increment,
+    clienteNome varchar(100),
+    clienteTelefone varchar(12),
+    clienteCpf varchar(11),
+    clienteEmail varchar(50),
+    clienteTemConvenio bool,
+    clienteNumeroConvenio varchar(100),
+    clienteNomeConvenio varchar(50),
+    primary key (clienteid)
+);
+
+
+create Table Clinica (
+	clinicaId int not null auto_increment,
+    clinicaNome varchar(100),
+    clinicaCnpj varchar(14),
+    clinicaTelefone varchar(13),
+    clinicaEndereco varchar(150),
+    primary key (clinicaId)
+);
+
+
+create Table Agendamento (
+	agendamentoId int not null auto_increment,
+        agendamentoSituacao char(1),
+        agendamentoData date,
+        primary key (agendamentoId),
+	foreign key (agendamentoId) references cliente(clienteId),
+	foreign key (agendamentoId) references clinica(clinicaId)
+);
--- CriacaoDb.sql
+++
@@ -1,3 +0,0 @@
-create database controleAgendamento;
-
-use controleAgendamento;
